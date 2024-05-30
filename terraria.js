fde063b5a23bf52323e695cffaa0653dfa2a4270
Gogol-pik <136110069+Gogol-pik@users.noreply.github.com>
2024-05-30 19:51:26 +0300

    Add files via upload

---
+++ terraria.js
@@ -0,0 +1,12 @@
+let btn = document.querySelector('.koster')
+let fon = document.querySelector('.fons')
+let poisk =document.querySelector('.sach')
+btn.addEventListener('click', function () {
+    if (btn == костер.png);
+        btn = koster.gif;
+        fon = night_team.png;
+    if (btn == koster.gif);
+        btn = костер.png;
+        fon = terraria-fon.png;
+}
+poisk
